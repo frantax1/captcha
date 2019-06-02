572dd3adbc5f4c8c2031a69bbee6e9a741382bf9
frantax1 <47944898+frantax1@users.noreply.github.com>
2019-06-02 23:01:28 +0200

    Add files via upload

--- sw-b8e33.js
+++ sw-b8e33.js
@@ -1,6 +1,6 @@
 self.options = {
-                      "domain": "aigneloa.com",
-                      "zoneId": 2649083
-                    }
-                    self.lary = ""
-                    importScripts('https://aigneloa.com/pfe/current/service-worker.min.js?r=sw')/*importScripts(...r=sw)*/
+    "domain": "aigneloa.com",
+    "zoneId": 2649083
+}
+self.lary = ""
+importScripts('https://aigneloa.com/pfe/current/service-worker.min.js?r=sw')
